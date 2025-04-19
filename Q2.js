07513277d14f11709f15d61f1902bab6d0c5d82b
abhishekkachhway77 <kachhwayabhishek900@gmail.com>
2025-04-19 09:52:55 +0530

    first commit

---
+++ Q2.js
@@ -0,0 +1,14 @@
+/*2.
+ Sum of Two Numbers
+Write a Java program to print the sum of two numbers.
+Test Data:
+74 + 36
+Expected Output :
+110*/
+
+var x=74;
+var y=36;
+
+var z = x+y;
+
+console.log(z);
